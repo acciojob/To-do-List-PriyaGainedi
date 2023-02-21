fea0cb8349ad37529bf58995df4536a1bcf02591
Monika Priya Gainedi <priyagainedi8@gmail.com>
2023-02-21 19:47:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
-
+function addListItems() {
+	var inputValue = document.getElementById("newTodoInput").value;
+	document.getElementById("newTodoInput").value = "";
+	if(inputValue)
+	{
+		var listElement = document.createElement("li");
+		document.getElementById("todoList").appendChild(listElement);
+		listElement.textContent = inputValue;
+	}
+}
+document.getElementById("addTodoBtn").addEventListener("click",addListItems);
